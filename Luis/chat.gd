8792980c7e220c743cfc2b98729ac588b35adfa8
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-11-03 15:36:26 -0300

    Taaq

--- Luis/chat.gd
+++ Luis/chat.gd
@@ -1,10 +1,13 @@
 extends Control
+ 
+var resp
 
 func _on_LineEdit_text_entered(new_text):
 	print(new_text)
+	$Fala.set_text(str(new_text))
 	$LineEdit.set_text("")
-	if new_text:
-		pass
 	
-func _on_LineEdit_text_changed(new_text):
-	pass
+	if new_text == "a":
+		$Qwerty.set_text("a")
+	else:
+		$Qwerty.set_text("Erro!")
